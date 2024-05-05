0059014af15375ccca79cf9c719e247f565a5545
Thamires Santos <106447459+thamiresantos@users.noreply.github.com>
2024-05-05 10:41:07 -0300

    função reset

--- javascript/script.js
+++ javascript/script.js
@@ -19,3 +19,18 @@ function pause(){
     clearInterval(cron);
 }
 
+//função reset
+function reset() {
+    let hora = 0;
+    let minuto = 0;
+    let segundo = 0;
+    let milissegundo = 0;
+
+    document.getElementById('horas').innerText = '00';
+    document.getElementById('minutos').innerText = '00';
+    document.getElementById('segundos').innerText ='00';
+    document.getElementById('milissegundos').innerText = '000';
+
+}
+
+
