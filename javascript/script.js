e261397d18ccc3bc22222cf7843b8b8a0c39add9
Thamires Santos <106447459+thamiresantos@users.noreply.github.com>
2024-05-05 10:39:04 -0300

    funcao iniciar

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,17 @@
 "use strict";
 
+//variáveis
 let hora = 0;
 let minuto = 0;
 let segundo = 0;
 let milissegundo = 0;
+
+let cron;
+
+
+//função iniciar
+function start() {
+    pause();
+    cron = setInterval(() => {timer();}, 10);
+}
 
